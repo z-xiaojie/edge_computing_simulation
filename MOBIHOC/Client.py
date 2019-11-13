2e17338e735d8f2085829c84b4cf0f6efe5702f6
z-xiaojie <xz326@hunter.cuny.edu>
2019-11-14 07:55:39 +0800

    1

--- MOBIHOC/Client.py
+++ MOBIHOC/Client.py
@@ -5,12 +5,12 @@ from Device import Device
 import threading
 import copy
 from Offloading_Mobihoc import *
-from Optimization import opt
+from Optimization import Optimization
 import time
 import struct
 
 
-class Helper:
+class Helper(Optimization):
     def __init__(self, host, start, end):
         self.host = host
         start1 = int(start)
@@ -29,83 +29,6 @@ class Helper:
     def reset_request_pool(self, number_of_user):
         self.request = [None for n in range(number_of_user)]
         self.finish = [0 for n in range(number_of_user)]
-
-    def search_cache(self, info, user_id, edge_id):
-        for s, d, config, task_id, k in self.cache:
-            if task_id != user_id or k != edge_id:
-                continue
-            selected = []
-            delta = []
-            for n in range(len(info["selection"])):
-                if info["selection"][n] == edge_id:
-                    selected.append(n)
-                    delta.append(info["opt_delta"][n])
-            if np.array_equal(s, selected) and np.array_equal(delta, d):
-                return config
-            else:
-                print(s, selected, d, delta)
-        return None
-
-    def opt(self, info):
-        target = info["who"]
-        validation = []
-        if not info["full"]:
-            feasible = [0]
-            small_data = target.DAG.jobs[0].input_data
-            for delta in range(1, len(target.DAG.jobs) - 1):
-                if target.DAG.jobs[delta].input_data < small_data:
-                    feasible.append(delta)
-                    small_data = target.DAG.jobs[delta].input_data
-                else:
-                    break
-            for delta in feasible:
-                info["Y_n"][target.task_id], info["X_n"][target.task_id], info["B"][target.task_id] = 0, 0, 0
-                for m in range(0, delta):
-                    info["Y_n"][target.task_id] += target.DAG.jobs[m].computation
-                for m in range(delta, target.DAG.length):
-                    info["X_n"][target.task_id] += target.DAG.jobs[m].computation
-                if delta == 0:
-                    info["B"][target.task_id] = target.DAG.jobs[delta].input_data
-                else:
-                    info["B"][target.task_id] = target.DAG.jobs[delta - 1].output_data
-                for k in range(info["number_of_edge"]):
-                    optimize = Offloading(info, k)
-                    info["selection"][target.task_id] = k
-                    info["opt_delta"][target.task_id] = delta
-                    config = self.search_cache(info, target.task_id, k)
-                    if config is None:
-                        config = optimize.start_optimize(delta=delta)
-                        selected = []
-                        partition_delta = []
-                        for n in range(info["number_of_user"]):
-                            if info["selection"][n] == k:
-                                selected.append(n)
-                                partition_delta.append(info["opt_delta"][n])
-                        self.cache.append(
-                            (selected, partition_delta, config, target.task_id, k))
-                        # print("saving", self.cache[-1])
-                    else:
-                        print("read from cache: get user info", target.task_id)
-                    if config is not None and (config[0] < target.local_only_energy or not target.local_only_enabled):
-                        validation.append({
-                            "edge": k,
-                            "config": config
-                        })
-        else:
-            delta = 0
-            info["Y_n"][target.task_id], info["X_n"][target.task_id], info["B"][target.task_id] = 0, 0, 0
-            for m in range(0, target.DAG.length):
-                info["X_n"][target.task_id] += target.DAG.jobs[m].computation
-            info["B"][target.task_id] = target.DAG.jobs[delta].input_data
-            for k in range(info["number_of_edge"]):
-                optimize = Offloading(info, k)
-                config = optimize.start_optimize(delta=delta)
-                if config is not None and (config[0] < target.local_only_energy or not target.local_only_enabled):
-                    validation.append({
-                        "edge": k,
-                        "config": config
-                    })
-        return validation, target
 
     def worker(self, info):
         validation, target = self.opt(info)
--- MOBIHOC/Optimization.py
+++ MOBIHOC/Optimization.py
@@ -1,28 +1,75 @@
 from Offloading_Mobihoc import Offloading
+import numpy as np
 
 
-def opt(info):
-    target = info["who"]
-    validation = []
-    if not info["full"]:
-        feasible = [0]
-        small_data = target.DAG.jobs[0].input_data
-        for delta in range(1, len(target.DAG.jobs) - 1):
-            if target.DAG.jobs[delta].input_data < small_data:
-                feasible.append(delta)
-                small_data = target.DAG.jobs[delta].input_data
-            else:
-                break
-        for delta in feasible:
+class Optimization:
+    def __init__(self):
+        self.cache = []
+
+    def search_cache(self, info, user_id, edge_id):
+        for s, d, config, task_id, k in self.cache:
+            if task_id != user_id or k != edge_id:
+                continue
+            selected = []
+            delta = []
+            for n in range(len(info["selection"])):
+                if info["selection"][n] == edge_id:
+                    selected.append(n)
+                    delta.append(info["opt_delta"][n])
+            if np.array_equal(s, selected) and np.array_equal(delta, d):
+                return config
+        return None
+
+    def opt(self, info):
+        target = info["who"]
+        validation = []
+        if not info["full"]:
+            feasible = [0]
+            small_data = target.DAG.jobs[0].input_data
+            for delta in range(1, len(target.DAG.jobs) - 1):
+                if target.DAG.jobs[delta].input_data < small_data:
+                    feasible.append(delta)
+                    small_data = target.DAG.jobs[delta].input_data
+                else:
+                    break
+            for delta in feasible:
+                info["Y_n"][target.task_id], info["X_n"][target.task_id], info["B"][target.task_id] = 0, 0, 0
+                for m in range(0, delta):
+                    info["Y_n"][target.task_id] += target.DAG.jobs[m].computation
+                for m in range(delta, target.DAG.length):
+                    info["X_n"][target.task_id] += target.DAG.jobs[m].computation
+                if delta == 0:
+                    info["B"][target.task_id] = target.DAG.jobs[delta].input_data
+                else:
+                    info["B"][target.task_id] = target.DAG.jobs[delta - 1].output_data
+                for k in range(info["number_of_edge"]):
+                    optimize = Offloading(info, k)
+                    info["selection"][target.task_id] = k
+                    info["opt_delta"][target.task_id] = delta
+                    config = self.search_cache(info, target.task_id, k)
+                    if config is None:
+                        config = optimize.start_optimize(delta=delta)
+                        selected = []
+                        partition_delta = []
+                        for n in range(info["number_of_user"]):
+                            if info["selection"][n] == k:
+                                selected.append(n)
+                                partition_delta.append(info["opt_delta"][n])
+                        self.cache.append(
+                            (selected, partition_delta, config, target.task_id, k))
+                    else:
+                        print("read from cache: get user info", target.task_id)
+                    if config is not None and (config[0] < target.local_only_energy or not target.local_only_enabled):
+                        validation.append({
+                            "edge": k,
+                            "config": config
+                        })
+        else:
+            delta = 0
             info["Y_n"][target.task_id], info["X_n"][target.task_id], info["B"][target.task_id] = 0, 0, 0
-            for m in range(0, delta):
-                info["Y_n"][target.task_id] += target.DAG.jobs[m].computation
-            for m in range(delta, target.DAG.length):
+            for m in range(0, target.DAG.length):
                 info["X_n"][target.task_id] += target.DAG.jobs[m].computation
-            if delta == 0:
-                info["B"][target.task_id] = target.DAG.jobs[delta].input_data
-            else:
-                info["B"][target.task_id] = target.DAG.jobs[delta - 1].output_data
+            info["B"][target.task_id] = target.DAG.jobs[delta].input_data
             for k in range(info["number_of_edge"]):
                 optimize = Offloading(info, k)
                 config = optimize.start_optimize(delta=delta)
@@ -31,19 +78,4 @@ def opt(info):
                         "edge": k,
                         "config": config
                     })
-    else:
-        delta = 0
-        info["Y_n"][target.task_id], info["X_n"][target.task_id], info["B"][target.task_id] = 0, 0, 0
-        for m in range(0, target.DAG.length):
-            info["X_n"][target.task_id] += target.DAG.jobs[m].computation
-        info["B"][target.task_id] = target.DAG.jobs[delta].input_data
-        for k in range(info["number_of_edge"]):
-            optimize = Offloading(info, k)
-            config = optimize.start_optimize(delta=delta)
-            if config is not None and (config[0] < target.local_only_energy or not target.local_only_enabled):
-                validation.append({
-                    "edge": k,
-                    "config": config
-                })
-    return validation, target
-
+        return validation, target
